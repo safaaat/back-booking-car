92509fca23a8722b6ae72cd0c59df03c2807b303
Muhammad Safaat <paatlupi01@gmail.com>
2025-07-24 16:41:18 +0700

    update

--- config/database.js
+++ config/database.js
@@ -1,20 +1,27 @@
-import { Sequelize } from "sequelize";
+import mysql from "mysql2";
 import dotenv from "dotenv";
+import { Sequelize } from "sequelize";
 dotenv.config();
 
-const db = new Sequelize(
-    process.env.DB_DBNAME,
-    process.env.DB_USERNAME,
-    process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_HOST,
-        dialect: 'mysql',
-        logging: false
-    }
-);
+const db = new Sequelize(process.env.DB_DBNAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    pool: {
+        max: 10,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    },
+    logging: false // Set to true if you want to see SQL queries in the console
+});
 
+// Test the connection
 db.authenticate()
-    .then(() => console.log("Database connected"))
-    .catch(err => console.error("DB connection failed:", err.message));
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
 
 export default db;
